2cd485fec5bccd8e12436c4223229fcb458b74a5
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2018-12-30 15:29:03 +0200

    Load nvm

--- bash/plugins.bash
+++ bash/plugins.bash
@@ -9,3 +9,7 @@ fi
 
 # Hook direnv into bash.
 eval "$(direnv hook bash)"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
