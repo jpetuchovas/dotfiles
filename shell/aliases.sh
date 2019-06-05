303d7c2211bfedf6a9f58f12eaa290f5e2abad09
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2019-06-05 14:24:47 +0300

    Remove an unused alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,7 +3,3 @@ alias grep='grep --color=auto'
 
 alias ll='ls --all --human-readable -l'
 alias la='ls --almost-all'
-
-# Add an "alert" alias for long running commands.  Use like so:
-# sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
