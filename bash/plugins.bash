fbc1573a6f74e566f63d3dc94d06d1f560a5a28d
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2019-06-05 14:20:51 +0300

    Remove nvm configuration

--- bash/plugins.bash
+++ bash/plugins.bash
@@ -9,7 +9,3 @@ fi
 
 # Hook direnv into bash.
 eval "$(direnv hook bash)"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
