ae6f298623b9b907c63788b1f209da7cb5184f32
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2019-07-18 13:07:35 +0300

    Remove unnecessary spaces

--- shell/external.sh
+++ shell/external.sh
@@ -4,5 +4,5 @@ export PYTHONDONTWRITEBYTECODE=true
 export NVM_DIR="${HOME}/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh" --no-use
 
-alias node='unalias node ; unalias npm ; nvm use default ; node $@'
-alias npm='unalias node ; unalias npm ; nvm use default ; npm $@'
+alias node='unalias node; unalias npm; nvm use default; node $@'
+alias npm='unalias node; unalias npm; nvm use default; npm $@'
