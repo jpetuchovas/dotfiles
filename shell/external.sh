b1219d2a30a3433a721553e7ed5c0765e9ad950f
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2019-07-18 09:43:35 +0300

    Fix nvm not loading and being slow

--- shell/external.sh
+++ shell/external.sh
@@ -2,4 +2,7 @@
 export PYTHONDONTWRITEBYTECODE=true
 
 export NVM_DIR="${HOME}/.nvm"
-[ -s " /usr/local/opt/nvm/nvm.sh" ] && . " /usr/local/opt/nvm/nvm.sh"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh" --no-use
+
+alias node='unalias node ; unalias npm ; nvm use default ; node $@'
+alias npm='unalias node ; unalias npm ; nvm use default ; npm $@'
