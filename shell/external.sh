d9f4509215888b6685e9deaa4acaa215c3a9aa5b
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2019-11-10 14:28:29 +0200

    Remove PYTHONDONTWRITEBYTECODE environment variable

--- shell/external.sh
+++ shell/external.sh
@@ -1,6 +1,3 @@
-# Prevent Python from creating .pyc files on the import of source modules.
-export PYTHONDONTWRITEBYTECODE=true
-
 export NVM_DIR="${HOME}/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh" --no-use
 
