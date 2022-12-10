eaa958be4b53af4292fe7a1d6d8c2888961e331d
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2022-12-10 20:28:47 +0200

    Add Neovim alias

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -1,6 +1,5 @@
-alias ls='ls -G'
-alias grep='grep --color=auto'
-
-alias ll='ls -ahl'
-
 alias dotfiles="git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME"
+alias grep='grep --color=auto'
+alias ll='ls -ahl'
+alias ls='ls -G'
+alias vim=nvim
