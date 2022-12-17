e4dd4ee9c6b72ddac43f2465b0f65b174d4184a9
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2022-12-17 10:56:33 +0200

    Imprve LSP config

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -10,6 +10,17 @@ lsp.ensure_installed({
   "tsserver",
 })
 
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
+end)
+
 lsp.nvim_workspace()
 
 lsp.setup()
+
+vim.diagnostic.config({virtual_text = true})
