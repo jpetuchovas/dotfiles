9301a624a29c47d0c138db30131085a35431625f
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2022-12-18 11:50:05 +0200

    Add file-specific options

---
+++ .config/nvim/after/ftplugin/python.lua
@@ -0,0 +1,3 @@
+vim.opt.colorcolumn = "72,88"
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,10 +1,10 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
 
 vim.opt.smartindent = true
 
