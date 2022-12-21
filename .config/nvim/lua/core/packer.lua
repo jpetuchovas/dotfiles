0097eb47670b74593e092d54bd55454eb7fb5ad6
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2022-12-21 21:10:22 +0200

    Add some lsp-zero requirements

--- .config/nvim/lua/core/packer.lua
+++ .config/nvim/lua/core/packer.lua
@@ -41,8 +41,12 @@ require("packer").startup(function(use)
       {"hrsh7th/nvim-cmp"},
       {"hrsh7th/cmp-buffer"},
       {"hrsh7th/cmp-path"},
+      {"saadparwaiz1/cmp_luasnip"},
       {"hrsh7th/cmp-nvim-lsp"},
       {"hrsh7th/cmp-nvim-lua"},
+
+      -- Snippets
+      {"L3MON4D3/LuaSnip"},
     }
   })
   use("jose-elias-alvarez/null-ls.nvim")
