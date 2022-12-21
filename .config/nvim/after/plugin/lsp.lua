c13556a3bd2a94a5bb52abcea67991f02df380bc
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2022-12-21 19:09:04 +0200

    Start completion sooner

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -4,8 +4,8 @@ lsp.preset("recommended")
 lsp.setup_nvim_cmp({
   sources = {
     {name = "path"},
-    {name = "nvim_lsp", keyword_length = 3},
-    {name = "buffer", keyword_length = 3},
+    {name = "nvim_lsp", keyword_length = 2},
+    {name = "buffer", keyword_length = 2},
   },
 })
 
