8188f9f0778bce04a612c6797ab1b963ebb06aee
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2022-12-21 21:31:45 +0200

    Set up Black and isort and format on save

--- .config/nvim/after/plugin/null_ls.lua
+++ .config/nvim/after/plugin/null_ls.lua
@@ -1,7 +1,28 @@
 local null_ls = require("null-ls")
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
   sources = {
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.isort,
     null_ls.builtins.formatting.prettier,
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({
+            bufnr = bufnr,
+            filter = function(clnt)
+                return clnt.name == "null-ls"
+            end,
+          })
+        end,
+      })
+    end
+  end,
 })
