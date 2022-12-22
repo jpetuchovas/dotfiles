19e1dbb2e51c249750e7a13ebdb9881550530450
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2022-12-22 07:26:49 +0200

    Improve how trailing whitespace is trimmed

--- .config/nvim/lua/core/formatting.lua
+++ .config/nvim/lua/core/formatting.lua
@@ -1,2 +1,9 @@
 -- Removes trailing whitespace on save.
-vim.api.nvim_create_autocmd("BufWritePre", {command = [[%s/\s\+$//e]]})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    local cursor_position = vim.api.nvim_win_get_cursor(0)
+    -- keeppatterns makes it so that the search history is not altered.
+    vim.cmd([[keeppatterns %s/\s\+$//e]])
+    vim.api.nvim_win_set_cursor(0, cursor_position)
+  end,
+})
