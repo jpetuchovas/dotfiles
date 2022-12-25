9fda2dd07964d20f04d0d2464a410e28771db88a
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2022-12-25 09:44:12 +0200

    Improve debugging shortcuts

--- .config/nvim/after/plugin/dap.lua
+++ .config/nvim/after/plugin/dap.lua
@@ -1,12 +1,18 @@
 local dap = require("dap")
 
-vim.keymap.set("n", "<leader>acn", dap.continue)
-vim.keymap.set("n", "<leader>atb", dap.toggle_breakpoint)
-vim.keymap.set("n", "<leader>asv", dap.step_over)
-vim.keymap.set("n", "<leader>asi", dap.step_into)
-vim.keymap.set("n", "<leader>ast", dap.step_out)
-vim.keymap.set("n", "<leader>arl", dap.run_last)
-vim.keymap.set("n", "<leader>atr", dap.terminate)
+vim.keymap.set("n", "<leader>ba", dap.toggle_breakpoint)
+vim.keymap.set("n", "<leader>bc", dap.continue)
+vim.keymap.set("n", "<leader>bh", dap.step_back)
+vim.keymap.set("n", "<leader>bj", dap.step_into)
+vim.keymap.set("n", "<leader>bk", dap.step_out)
+vim.keymap.set("n", "<leader>bl", dap.step_over)
+vim.keymap.set("n", "<leader>b.", dap.run_to_cursor)
+vim.keymap.set("n", "<leader>br", dap.run_last)
+vim.keymap.set("n", "<leader>bL", dap.list_breakpoints)
+vim.keymap.set("n", "<leader>bJ", dap.down)
+vim.keymap.set("n", "<leader>bK", dap.up)
+vim.keymap.set("n", "<leader>bx", dap.clear_breakpoints)
+vim.keymap.set("n", "<leader>bX", dap.terminate)
 
 local dapui = require("dapui")
 dapui.setup({
@@ -24,7 +30,7 @@ dapui.setup({
       step_out = "out",
       step_back = "back",
       run_last = "last",
-      terminate = "terminate",
+      terminate = "X",
     },
   },
 })
