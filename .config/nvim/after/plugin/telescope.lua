82c1dd325f53f1e4b4c7bff097d94d5577ace136
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2023-09-10 15:11:16 +0300

    Get rid of <space>ff

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,7 @@
 require("telescope").load_extension("fzf")
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files)
-vim.keymap.set("n", "<C-p>", builtin.git_files)
+vim.keymap.set("n", "<C-p>", builtin.find_files)
 vim.keymap.set("n", "<leader>fs", function()
   builtin.grep_string({search = vim.fn.input("Grep > ")})
 end)
