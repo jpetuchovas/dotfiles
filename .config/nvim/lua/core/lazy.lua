a920a80ffe8b03ee3a845fab078ab5ae60d51ff8
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2025-04-01 13:14:58 +0300

    Update lazy plugins

--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -33,32 +33,22 @@ local plugins = {
   "tpope/vim-fugitive",
   "lewis6991/gitsigns.nvim",
 
+  -- LSP Support.
+  "neovim/nvim-lspconfig",
   {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v4.x",
-    dependencies = {
-      -- LSP Support.
-      {"neovim/nvim-lspconfig"},
-      {
-        "williamboman/mason.nvim",
-        build = function()
-          pcall(vim.cmd, "MasonUpdate")
-        end,
-      },
-      {"williamboman/mason-lspconfig.nvim"},
-
-      -- Autocompletion.
-      {"hrsh7th/nvim-cmp"},
-      {"hrsh7th/cmp-buffer"},
-      {"hrsh7th/cmp-path"},
-      {"saadparwaiz1/cmp_luasnip"},
-      {"hrsh7th/cmp-nvim-lsp"},
-      {"hrsh7th/cmp-nvim-lua"},
-
-      -- Snippets
-      {"L3MON4D3/LuaSnip"},
-    }
+    "williamboman/mason.nvim",
+    build = function()
+      pcall(vim.cmd, "MasonUpdate")
+    end,
   },
+  "williamboman/mason-lspconfig.nvim",
+
+  -- Autocompletion.
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-nvim-lsp",
+
   "mfussenegger/nvim-lint",
   "stevearc/conform.nvim",
   {
